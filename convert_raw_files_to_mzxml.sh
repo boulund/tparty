e1ecda9094eac565fdcc6958e746a24498885bad
Fredrik Boulund <fredrik.boulund@chalmers.se>
2015-12-19 17:16:05 +0100

    Changed default ReAdW settings to NOT use --centroid. Also added some minor adjustments to the script.

--- convert_raw_files_to_mzxml.sh
+++ convert_raw_files_to_mzxml.sh
@@ -2,14 +2,14 @@
 # Convert RAW files in RAW_DIR to mzXML files in MZXML_DIR
 # Fredrik Boulund 2015
 
-# Set important PATHs
+# Set important paths and parameters
 # This script expects a working Wine32 prefix in WINEPREFIX
 export READW=/home/boulund/research/TTT/src/ReAdW/bin/ReAdW.201510.xcalibur.exe
 export WINEPREFIX=/home/boulund/research/TTT/wine32/
-export GNU_PARALLEL=/local/bin/parallel
+GNU_PARALLEL=/local/bin/parallel
+NUM_CPU=20
 
-# Check if user specified DIR to be converted
-#####
+# Check that enough command line arguments were specified
 if [ $# -lt 1 ]
 then
 	echo "Usage: convert_raw_files_to_mzxml.sh <RAW_DIR> <MZXML_DIR>"
@@ -26,10 +26,10 @@ then
 fi
 
 # Perform RAW file conversions in parallel using GNU parallel
-$GNU_PARALLEL -j 20 'if [ ! -e $MZXML_DIR/{/.}.mzXML.gz ]; 
+$GNU_PARALLEL -j $NUM_CPU 'if [ ! -e $MZXML_DIR/{/.}.mzXML.gz ]; 
 	then
 		echo "Converting {} to $MZXML_DIR/{/.}.mzXML.gz";
-		wine $READW --centroid --nocompress --gzip {} $MZXML_DIR/{/.}.mzXML.gz;
+		wine $READW --nocompress --gzip {} $MZXML_DIR/{/.}.mzXML.gz;
 	#else
 	#	echo "Skipping {}"
 	fi' ::: ${RAW_DIR}/*.raw
