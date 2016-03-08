13238e0113bfe2f138318855babb28d1ce97cb42
Fredrik Boulund <fredrik.boulund@chalmers.se>
2016-03-08 06:43:41 +0100

    Added flock to crontab script.

--- snakemake_crontab_script.sh
+++ snakemake_crontab_script.sh
@@ -2,11 +2,22 @@
 # Snakemake crontab script
 # Fredrik Boulund 2016
 
-source $HOME/.bash_profile          # Load environment variables required to activate conda environment
-source activate proteotyping        # Activate conda environment
-cd /storage/TTT/                    # Change workdir to analysis dir
-export PATH=/storage/TTT/bin:$PATH  # Add TTT_pipeline executables to path
-snakemake \
+# Lockfile used to indicate if snakemake workflow is currently running
+LOCKFILE=/storage/TTT/snakemake_running.lock
+
+# Load environment variables required to activate conda environment
+source $HOME/.bash_profile          
+# Activate conda environment
+source activate proteotyping        
+# Change workdir to analysis dir
+cd /storage/TTT/                    
+# Add TTT_pipeline executables to path
+export PATH=/storage/TTT/bin:$PATH  
+
+# Run snakemake only if it isn't already currently running.
+# flock creates a lockfile that indicates if the workflow is currently in
+# progress.
+flock -n $LOCKFILE snakemake \
 	--snakefile TTT_pipeline.snakefile \
 	--configfile TTT_pipeline_snakemake_config.yaml \
 	--cores 22
