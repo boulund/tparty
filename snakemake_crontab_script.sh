d5855d95e5c201401bc1dcc03435e7c8c287aeba
Fredrik Boulund <fredrik.boulund@chalmers.se>
2016-03-11 10:04:48 +0100

    Modified flock lock file.

--- snakemake_crontab_script.sh
+++ snakemake_crontab_script.sh
@@ -3,7 +3,7 @@
 # Fredrik Boulund 2016
 
 # Lockfile used to indicate if snakemake workflow is currently running
-LOCKFILE=/storage/TTT/snakemake_running.lock
+export LOCKFILE=/storage/TTT/.TTT_snakemake.lock
 
 # Load environment variables required to activate conda environment
 source $HOME/.bash_profile          
@@ -15,10 +15,12 @@ cd /storage/TTT/
 export PATH=/storage/TTT/bin:$PATH  
 
 # Run snakemake only if it isn't already currently running.
-# flock creates a lockfile that indicates if the workflow is currently in
-# progress.
-flock -n $LOCKFILE snakemake \
-	--snakefile TTT_pipeline.snakefile \
-	--configfile TTT_pipeline_snakemake_config.yaml \
-	--cores 22 \
-	$1 
+# flock creates a lockfile that, when open, indicates if the workflow in
+# currently in progress. flock detects if the file is already opened and 
+# the -n command makes it do nothing if snakemake is already running.
+flock -n $LOCKFILE \
+	snakemake \
+		--snakefile TTT_pipeline.snakefile \
+		--configfile TTT_pipeline_snakemake_config.yaml \
+		--cores 22 \
+		$1
